92e076b105672aac5fc7f4767c885bdc3bdefe71
anlin-yang <anlin0414@outlook.com>
2015-08-05 15:29:11 +0800

    Code optimization agein.

--- main/models/form-analyser.js
+++ main/models/form-analyser.js
@@ -4,57 +4,31 @@ FormAnalyser.prototype.formSerializer = function(form) {
   var result = [];
 
   [].forEach.call(form, function(elem) {
-    var isExist;
-    isExist = result.some(function(val) {
-      return val.name === elem.name;
-    });
+    if ((elem.type === 'radio' && elem.checked === false) || (elem.type === 'checkbox' && elem.checked === false)) {
 
-    switch (elem.type) {
-      case 'text':
-        if (isExist) {
-          var temp2 = result.filter(function(item) {
-            return item.name === elem.name;
-          });
-          temp2[0].value.push(elem.value);
-        } else {
-          result.push({
-            name: elem.name,
-            type: elem.type,
-            value: [elem.value]
-          });
-        }
-        break;
-
-      case 'radio':
-        if (isExist === false) {
-          if (elem.checked === true) {
-            result.push({
-              name: elem.name,
-              type: elem.type,
-              value: elem.value
-            });
+    } else {
+      var isExist;
+      isExist = result.some(function(val) {
+        return val.name === elem.name;
+      });
+      if (isExist === false) {
+        result.push({
+          name: elem.name,
+          type: elem.type,
+          value: [elem.value]
+        });
+      } else {
+        result.forEach(function(item) {
+          if (item.name === elem.name) {
+            item.value.push(elem.value);
           }
-        }
-        break;
-        
-      case 'checkbox':
-        if (isExist) {
-          if (elem.checked === true) {
-            var temp3 = result.filter(function(item) {
-              return item.name === elem.name;
-            });
-            temp3[0].value += elem.value;
-          }
-        } else {
-          if (elem.checked === true) {
-            result.push({
-              name: elem.name,
-              type: elem.type,
-              value: elem.value
-            });
-          }
-        }
-        break;
+        });
+      }
+    }
+  });
+  result.forEach(function(val) {
+    if (val.type === 'radio' || val.type === 'checkbox') {
+      val.value = val.value.join("");
     }
   });
   return result;
